27e09e2469c84b4740904f63a177f394b6fe95a6
Liza <richardaddy333@gmail.com>
2023-01-25 17:47:21 +0300

    feat: add a check for an empty string, add an enter key press

---
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -0,0 +1,87 @@
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import Greeting from './Greeting'
+import { UserType } from './HW3'
+
+type GreetingContainerPropsType = {
+    users: Array<UserType> // need to fix any
+    addUserCallback: (name: string)=>void // need to fix any
+}
+
+export const pureAddUser = (name: string, setError: (error1: string)=>void, setName: (name1: string)=>void, addUserCallback: (name: string)=>void) => {
+    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+    if (name.trim() === '') {
+        return setError('Ошибка! Введите имя!')
+    }
+    addUserCallback(name.trim());
+    setName('')
+
+    // if (name.length === 0) {
+    //     setError('Ошибка! Введите имя!')
+    // } else {
+    //     addUserCallback(name)
+    //     setName('')
+    // }
+}
+
+export const pureOnBlur = (name: string, setError: (error1: string)=>void) => { // если имя пустое - показать ошибку
+    if (name.trim() === '') {
+        return setError('Ошибка! Введите имя!')
+    }
+}
+
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: ()=>void) => { // если нажата кнопка Enter - добавить
+    if (e.key === 'Enter') {
+        addUser()
+    }
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+    users,
+    addUserCallback,
+}) => {
+    // деструктуризация пропсов
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value) // need to fix
+
+        error && setError('')
+    }
+
+    //это функция стрелочник - она всего лишь получает
+    //сигнал из компоненты <Greeting/>!!!!! и вызывает pureAddUser (с кучей аргументов)!!!!!
+    const addUser = () => {
+        pureAddUser(name, setError, setName, addUserCallback)
+    }
+
+    const onBlur = () => {
+        pureOnBlur(name, setError)
+    }
+
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        pureOnEnter(e, addUser)
+    }
+
+    const totalUsers = users.length // need to fix
+    const lastUserName = totalUsers !==0 ? users[totalUsers - 1].name : 'User' // need to fix
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            onBlur={onBlur}
+            onEnter={onEnter}
+            error={error}
+            totalUsers={totalUsers}
+            lastUserName={lastUserName}
+        />
+    )
+}
+
+export default GreetingContainer
