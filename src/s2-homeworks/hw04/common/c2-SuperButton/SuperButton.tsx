3df4a5e38813c260c37c6116009487d1f2cdade2
Liza <richardaddy333@gmail.com>
2023-02-17 19:04:29 +0300

    feat: add sidebars and routes

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,7 +18,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = `${s.button} ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}
-            ${disabled ? s.disabled : ''}`
+            ${disabled ? s.disabled : ''} ${className ? ' ' + className : ''}`
         // + (disabled
         //         ? ...
         //         : xType === 'red'
