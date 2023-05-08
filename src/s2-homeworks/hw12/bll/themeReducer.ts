22135836fc35a78b69fb4044c3160e39673a5116
Liza <richardaddy333@gmail.com>
2023-05-08 19:54:41 +0300

    feat: add theme

---
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -0,0 +1,25 @@
+type StateType = {
+    themeId: number
+}
+
+const initState = {
+    themeId: 1,
+}
+
+export const themeReducer = (state: StateType = initState, action: changeThemeIdType): StateType => { // fix any
+    switch (action.type) {
+        // дописать
+        case "SET_THEME_ID": {
+            return {...state, themeId: action.id}
+        }
+        default:
+            return state
+    }
+}
+
+type changeThemeIdType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
+export const changeThemeId = (id: number):  changeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
